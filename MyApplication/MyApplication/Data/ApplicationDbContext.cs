7f8fdc61734877168a82f1b246753f228111ae05
Saquib02 <mohammadsaquib194@gmail.com>
2023-06-02 20:40:43 +0530

    creating database and connecting with server

---
+++ MyApplication/MyApplication/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyApplication.Models;
+using System.Data.Common;
+
+namespace MyApplication.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)  : base(options)
+        {   
+            
+        }           
+        public DbSet<Category> Categories { get; set; }
+    }
+
+}
---
+++ MyApplication/MyApplication/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyApplication.Models
+{
+    public class Category
+    {
+        [Key]
+        public int id { get; set; }
+
+        [Required]
+        public string name { get; set; }
+
+        public int DisplayeOrder { get; set; }
+
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+    }
+}
